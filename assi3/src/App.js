dbd5233e076be3ce9ee70dae72cab03cbe5a55ee
Saurabh0241 <Saurabhsakhare0241@gmail.com>
2023-04-20 22:34:23 +0530

    Done

---
+++ assi3/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+ import { Invitation } from './Invitation/invitation';
+import { Fragment } from 'react';
+function App() {
+  return (
+    
+    <Fragment>
+        <Invitation
+        birthday= 'Birthday party Invitation '
+        party=  'birthday party'
+        email=  'jaGdish@gmail.com'
+        
+        name=  ' jaGdish'
+        Avenue= 'Green field Avenue'
+        name1= 'Ritu'
+        name2= 'Saurabh'
+        name3= ' Kartik'
+        />
+      </Fragment>
+    
+  );
+}
+
+export default App;
