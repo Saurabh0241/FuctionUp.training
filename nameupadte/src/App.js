1a5c55cb7d0b1db31a25fcac12fa22808bd5ed53
Saurabh0241 <Saurabhsakhare0241@gmail.com>
2023-04-26 23:51:41 +0530

    Done

---
+++ nameupadte/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Nameupdate } from './Component/namupadte';
+
+function App() {
+  return (
+    <div className="App">
+      <Nameupdate/>
+    </div>
+  );
+}
+
+export default App;
