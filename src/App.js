61c7d1f475b0b011c89f39ce02df4812ad2a1b52
Saurabh0241 <Saurabhsakhare0241@gmail.com>
2023-04-26 13:03:14 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,57 @@
-  import logo from './logo.svg';
-  import './App.css';
-  import { Fragment } from 'react';
-  import { Button } from './Button/button';
-  import { Benfits } from './Benifits/benifits';
-  
-  
-  function App() {
-    return (
-      <Fragment>
-      <div className="App">
-     <img src={logo} className="App-logo" alt="logo" />
-        <header className="App-header">
-           <Benfits/>
-           <Button/>
-        </header>
-      </div>
-      </Fragment>
-    );
-  }
+import './App.css';
+import Productcard from './components/productcards';
+import React from 'react';
 
-  export default App;
+
+const  data = [{
+  productPic: "https://tse4.mm.bing.net/th?id=OIP.5DkTfmZhmkb34y6tzbb4fAHaE_&pid=Api&P=0",
+  productName: " Air Jorden",
+  productDescription: "This shoe is Air Jorden very much trending.",
+  Price: "$65",
+  Rating: "4.5",
+  alert : "product1"
+
+},
+{
+  productPic: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkBq0v4aYt67mjjPO8f-8pSd4Zhn2tzgWbqqHZhV-w&s",
+  productName: " Johnson ",
+  productDescription: "This Johnson watch is very  much trending",
+  Price: "$85",
+  Rating: "4.5",
+  alert : "product2"
+},
+
+{
+  productPic: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGVhZHBob25lc3xlbnwwfHwwfHw%3D&w=1000&q=80",
+  productName: "boAt Headphones", 
+  productDescription: "This Headphones is very much   trending.",
+  Price: "$65",
+  Rating: "4.5",
+  alert : "product3"
+},
+{
+
+  productPic: "https://cdn.shopify.com/s/files/1/0490/6011/8686/products/CVC-Sunscreen.jpg?v=1665755509",
+  productName: " Sunscreen",
+  productDescription: "This Sunscreen is very much trending.",
+  Price: "$65",
+  Rating: "4.5",
+  alert : "product4"
+},
+{
+  productPic: "https://tse2.mm.bing.net/th?id=OIP.m26yupS4vCSbyhAo32Ni2gHaFN&pid=Api&P=0",
+  productName: " Nike ",
+  productDescription: "This nike shoe is very much trending.",
+  Price: "$65",
+  Rating: "4.5",
+  alert : "product5"
+}];
+
+function App() {
+  return (
+    <div className="App">
+      <Productcard data={data}/>
+    </div>
+  );
+}
+export default App
--- src/Button/button.js
+++
@@ -1,9 +0,0 @@
-export function Button()
-{
-    return (
-        <button>
-            Get Stared ....
-        </button>
-
-    )
-}
