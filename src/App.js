c86fd37f6e77871fadf6c212beac15ff17ca335b
Saurabh0241 <Saurabhsakhare0241@gmail.com>
2023-04-26 22:34:51 +0530

    Done

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { Count } from './Component/count';
+
+function App() {
+
+
+return(
+<div className='count'>
+<Count/>
+</div>
+)
+  
+}
+
+export default App;
