fffac5481f4990c4f134d7c825a196bd12d6589f
Saurabh0241 <Saurabhsakhare0241@gmail.com>
2023-05-02 23:49:37 +0530

    Done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CardMaking from './atom/list';
+
+function App() {
+  return (
+    <div className="App">
+      <CardMaking/>
+    </div>
+  );
+}
+
+export default App;
