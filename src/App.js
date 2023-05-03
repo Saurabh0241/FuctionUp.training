4d231277328aba429bfcd6ccd90c7ec41db45d07
Saurabh0241 <Saurabhsakhare0241@gmail.com>
2023-05-04 01:51:34 +0530

    Done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Otpgenrate from './Componats/Atom/list';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Otpgenrate/>
+      
+    </div>
+  );
+}
+
+export default App;
