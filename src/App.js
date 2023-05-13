d03dfa95c9121f5f87c4f7afdea04de7b000c647
Saurabh0241 <Saurabhsakhare0241@gmail.com>
2023-05-13 10:43:46 +0530

    TODOLIST

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Crud from './Components/Organisms/Todolist';
+
+
+function App() {
+  return (
+    <div className="App">
+    <Crud/>
+    </div>
+  );
+}
+
+export default App;
