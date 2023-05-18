9e4f51fd8154cd411723f3215a6df569d288229f
Saurabh0241 <Saurabhsakhare0241@gmail.com>
2023-05-18 14:04:56 +0530

    Quiz app

---
+++ quizapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import QuizTest from './QuizComponents/Quizapp';
+
+function App() {
+  return (
+    <div className="App">
+    <QuizTest/>
+    </div>
+  );
+}
+
+export default App;
